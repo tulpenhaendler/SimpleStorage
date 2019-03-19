0d8c169208a31d445a04ef96933de0658d1e2f75
johann <mail@johanntanzer.at>
2019-03-19 13:38:36 +0100

    add update chan

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module gitlab.com/tulpenhaendler/simplestore
+
+require (
+	github.com/cstockton/go-conv v0.0.0-20170524002450-66a2b2ba36e1 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/go-fsnotify/fsnotify v0.0.0-20180321022601-755488143dae // indirect
+	golang.org/x/sys v0.0.0-20190318195719-6c81ef8f67ca // indirect
+)
--- store.go
+++ store.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/cstockton/go-conv"
+	"github.com/fsnotify/fsnotify"
 	"io/ioutil"
 	"log"
 	"os"
@@ -19,15 +20,13 @@ type Config struct {
 
 type Storage struct {
 	StorageFile string
-	lock *sync.Mutex
-	data map[string]string
+	lock        *sync.Mutex
+	data        map[string]string
+	notify      []chan struct{}
 }
-
-
 
 func NewSimpleStorage(name string, conf *Config) *Storage {
 	s := Storage{}
-
 
 	// Chose StorageFile
 	if envdir := os.Getenv("STORAGE_DIR"); envdir != "" {
@@ -40,10 +39,11 @@ func NewSimpleStorage(name string, conf *Config) *Storage {
 			fmt.Errorf("Cant create Storage Directory")
 			return nil
 		}
+		s.StorageFile = conf.StorageDir + "/storage"
 	} else {
 		usr, err := user.Current()
 		if err != nil {
-			log.Fatal( err )
+			log.Fatal(err)
 		}
 		dir := usr.HomeDir + "/." + name
 		if e := s.testDir(dir); e != nil {
@@ -53,45 +53,81 @@ func NewSimpleStorage(name string, conf *Config) *Storage {
 		s.StorageFile = dir + "/storage"
 	}
 
-
 	// make sure File exists
-	if _, err := os.Stat(s.StorageFile); os.IsNotExist(err){
+	if _, err := os.Stat(s.StorageFile); os.IsNotExist(err) {
 		if _, err := os.Create(s.StorageFile); err != nil {
 			fmt.Errorf("Cant create Storage Directory")
 			return nil
 		}
-
 	}
 
 	s.lock = &sync.Mutex{}
 	s.read()
-
+	go s.watchFileChange()
 	return &s
 }
 
+func (this *Storage) GetUpdateChan() chan struct{} {
+	res := make(chan struct{})
+	this.notify = append(this.notify, res)
+	return res
+}
 
-func (this *Storage) read(){
-	data,err := ioutil.ReadFile(this.StorageFile)
+func (this *Storage) sendNotify() {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	for _, c := range this.notify {
+		c <- struct{}{}
+	}
+}
+
+func (this *Storage) watchFileChange() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+	//
+
+	if err := watcher.Add(this.StorageFile); err != nil {
+		fmt.Println("ERROR ", err)
+	}
+
+	for {
+		select {
+		case <-watcher.Events:
+			this.read()
+			this.sendNotify()
+
+		case err := <-watcher.Errors:
+			fmt.Println("ERROR", err)
+		}
+	}
+
+}
+
+func (this *Storage) read() {
+	data, err := ioutil.ReadFile(this.StorageFile)
 	if err != nil {
 		this.data = map[string]string{}
 		return
 	}
-	if err = json.Unmarshal(data,&this.data); err != nil {
+	if err = json.Unmarshal(data, &this.data); err != nil {
 		this.data = map[string]string{}
 		return
 	}
 }
 
-func (this *Storage) save(){
+func (this *Storage) save() {
 	this.lock.Lock()
-	data,_ := json.Marshal(&this.data)
-	err := ioutil.WriteFile(this.StorageFile,data,644)
+	data, _ := json.Marshal(&this.data)
+	err := ioutil.WriteFile(this.StorageFile, data, 644)
 	if err != nil {
 		fmt.Println("Error saving Data to ", this.StorageFile)
 	}
 	this.lock.Unlock()
+	this.sendNotify()
 }
-
 
 func (this *Storage) testDir(dir string) error {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
@@ -100,182 +136,173 @@ func (this *Storage) testDir(dir string) error {
 	return nil
 }
 
-
-
 // Setters
 
-
-func (this *Storage) StoreInterface(key string,val interface{} )  {
-	bytes,_ := json.Marshal(val)
+func (this *Storage) StoreInterface(key string, val interface{}) {
+	bytes, _ := json.Marshal(val)
 	this.data[key] = string(bytes)
 	this.save()
 }
 
-
-func (this *Storage) StoreString(key, value string)  {
+func (this *Storage) StoreString(key, value string) {
 	this.data[key] = value
 	this.save()
 }
 
-func (this *Storage) StoreInt(key string, value int)  {
+func (this *Storage) StoreInt(key string, value int) {
 	this.data[key] = strconv.Itoa(value)
 	this.save()
 
 }
 
-func (this *Storage) StoreUint(key string, value uint)  {
-	v,_ := conv.String(value)
+func (this *Storage) StoreUint(key string, value uint) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreInt8 (key string, value int8){
-	v,_ := conv.String(value)
+func (this *Storage) StoreInt8(key string, value int8) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreInt32 (key string, value int32){
-	v,_ := conv.String(value)
+func (this *Storage) StoreInt32(key string, value int32) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreInt64 (key string, value int64){
-	v,_ := conv.String(value)
+func (this *Storage) StoreInt64(key string, value int64) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreuInt8 (key string, value uint8){
-	v,_ := conv.String(value)
+func (this *Storage) StoreuInt8(key string, value uint8) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreUint32 (key string, value uint32){
-	v,_ := conv.String(value)
+func (this *Storage) StoreUint32(key string, value uint32) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreUint64 (key string, value uint64){
-	v,_ := conv.String(value)
+func (this *Storage) StoreUint64(key string, value uint64) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreFloat32 (key string, value float32){
-	v,_ := conv.String(value)
+func (this *Storage) StoreFloat32(key string, value float32) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreFloat64 (key string, value float64){
-	v,_ := conv.String(value)
+func (this *Storage) StoreFloat64(key string, value float64) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreComplex64 (key string, value complex64){
-	v,_ := conv.String(value)
+func (this *Storage) StoreComplex64(key string, value complex64) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
 
-func (this *Storage) StoreComplex128 (key string, value complex128){
-	v,_ := conv.String(value)
+func (this *Storage) StoreComplex128(key string, value complex128) {
+	v, _ := conv.String(value)
 	this.data[key] = v
 	this.save()
 }
-
 
 // Getters
 
 func (this *Storage) GetInterface(key string, value interface{}) error {
-	if val,ok := this.data[key]; ok {
-		return json.Unmarshal([]byte(val),value)
+	if val, ok := this.data[key]; ok {
+		return json.Unmarshal([]byte(val), value)
 	}
 	return errors.New("Key not found")
 }
 
-func (this *Storage) GetString(key string) (string,error){
-	if val,ok := this.data[key]; ok {
-		return val,nil
+func (this *Storage) GetString(key string) (string, error) {
+	if val, ok := this.data[key]; ok {
+		return val, nil
 	}
-	return "",errors.New("Key not found")
+	return "", errors.New("Key not found")
 }
 
-
-func (this *Storage) GetInt(key string) (int,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetInt(key string) (int, error) {
+	if val, ok := this.data[key]; ok {
 		return strconv.Atoi(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetInt8 (key string) (int8,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetInt8(key string) (int8, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Int8(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-
-func (this *Storage) GetInt32 (key string) (int32,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetInt32(key string) (int32, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Int32(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetInt64 (key string) (int64,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetInt64(key string) (int64, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Int64(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetUint (key string) (uint,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetUint(key string) (uint, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Uint(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetUint8 (key string) (uint8,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetUint8(key string) (uint8, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Uint8(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetUint32 (key string) (uint32,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetUint32(key string) (uint32, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Uint32(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetUint64 (key string) (uint64,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetUint64(key string) (uint64, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Uint64(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-func (this *Storage) GetFloat32 (key string) (float32,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetFloat32(key string) (float32, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Float32(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
 
-
-func (this *Storage) GetFloat64 (key string) (float64,error){
-	if val,ok := this.data[key]; ok {
+func (this *Storage) GetFloat64(key string) (float64, error) {
+	if val, ok := this.data[key]; ok {
 		return conv.Float64(val)
 	}
-	return 0,errors.New("Key not found")
+	return 0, errors.New("Key not found")
 }
-
